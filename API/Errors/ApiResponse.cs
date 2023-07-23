ddb3e906d68a9e30f88dd8b0826a747d2a13ba05
blake-gifford <blakegifford01@gmail.com>
2023-07-22 22:15:53 -0600

    adding api error handling

---
+++ API/Errors/ApiResponse.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Errors
+{
+    public class ApiResponse
+    {
+        public ApiResponse(int statusCode, string message = null)
+        {
+            StatusCode = statusCode;
+            Message = message ?? GetDefaultMessageForStatusCode(statusCode);
+        }
+
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+
+        private string GetDefaultMessageForStatusCode(int statusCode)
+        {
+            return statusCode switch
+            {
+                400 => "You have made a bad request",
+                401 => "You are not authorized",
+                404 => "No resource found",
+                500 => "A server error has occurred",
+                _ => null
+            };
+        }
+    }
+}
